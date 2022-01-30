8e357a992b6e1b2aadd3ef8a78205dbe51029fdd
hoopsgators <hoopsgators@gmail.com>
2022-01-30 11:48:22 -0800

    payments begin

---
+++ payments/src/app.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import { json } from "body-parser";
+import "express-async-errors";
+
+import { errorHandler, NotFoundError, currentUser } from "@hoopstix/common";
+import cookieSession from "cookie-session";
+
+const app = express();
+
+app.set("trust proxy", true);
+app.use(json());
+app.use(
+  cookieSession({ signed: false, secure: process.env.NODE_ENV !== "test" })
+);
+
+app.all("*", async (req, res) => {
+  throw new NotFoundError();
+});
+app.use(errorHandler);
+
+export { app };
